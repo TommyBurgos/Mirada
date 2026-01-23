6293c0dc69240b1b1999a7ebaf6f606cbfbdb7b7
tommyBurgos <trburgos@espol.edu.ec>
2026-01-23 13:24:48 -0500

    Nuevo intento de crear superusuario 2

--- create_superuser.py
+++ create_superuser.py
@@ -1,5 +1,8 @@
 import os
 import django
+import sys
+
+print("🚀 Iniciando create_superuser.py", flush=True)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
@@ -8,9 +11,15 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+print("🔍 Modelo de usuario:", User, flush=True)
+print("🔍 DB ENGINE:", os.environ.get("DATABASE_URL"), flush=True)
+
 USERNAME = "admin"
 EMAIL = "admin@mirada.local"
 PASSWORD = "admin1234"
+
+qs = User.objects.filter(username=USERNAME)
+print("🔎 Usuarios encontrados con username=admin:", qs.count(), flush=True)
 
 user, created = User.objects.get_or_create(
     username=USERNAME,
@@ -21,9 +30,11 @@ user, created = User.objects.get_or_create(
     }
 )
 
+print("🆕 Usuario creado?", created, flush=True)
+
 user.set_password(PASSWORD)
 user.is_staff = True
 user.is_superuser = True
 user.save()
 
-print("✅ Superusuario listo / contraseña reseteada")
+print("✅ Usuario guardado correctamente", flush=True)
